a9109f060ec4a821550866ede6e47503e680d404
sorbetes6 <reuelgumawid6@gmail.com>
2024-05-31 01:39:24 +0800

    fixed mooe  record display

--- app/Livewire/Mooe.php
+++ app/Livewire/Mooe.php
@@ -2,15 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\Mooe as MooeModel; // Import the Mooe model
 use Livewire\Component;
 
 class Mooe extends Component
 {
+    public $load_mooe;
+    public $college_office = ['CASBE', 'CBA', 'CA', 'CTHM', 'CEng', 'CISTM', 'CHASS', 'CED', 'CN', 'CPT', 'CS', 'CL', 'GSL', 'CM', 'CPA'];
 
-    public $load_mooe = [];
+    public function mount()
+    {
+        // Fetch data from the database and create model instances
+        $mooesData = MooeModel::all()->toArray();
 
-    public function mount() {
-        $this->load_mooe= Mooe::all();
+        $this->load_mooe = collect($mooesData)->map(function ($mooeData) {
+            return new MooeModel($mooeData);
+        });
     }
 
     public function render()
