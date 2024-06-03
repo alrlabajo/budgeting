b0917399f37a69b4e0c1defd3779206754a8ee20
alrlabajo <alrlabajo@gmail.com>
2024-06-03 22:44:56 +0800

    added pdf controller + budget report button

--- routes/web.php
+++ routes/web.php
@@ -40,45 +40,33 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/chart', Chart::class);
 
-Route::get('/settings', Settings::class);
-
+//Record Routes
 Route::get('/activity-justification', ActivityJustification::class);
-
-Route::get('/activity-justification-form', ActivityJustificationForm::class);
-
 Route::get('/personal-services', PersonalServices::class);
-
-Route::get('/personal-services-form', PersonalServicesForm::class);
-
 Route::get('/personnel-schedule', PersonnelSchedule::class);
-
-Route::get('/personnel-schedule-form', PersonnelScheduleForm::class);
-
 Route::get('/MOOE', Mooe::class);
-
-Route::get('/MOOE-form', MaintenanceForm::class);
-
 Route::get('/capital-outlay', LoadCapitalOutlay::class);
-
-Route::get('/capital-outlay-form', CapitalOutlayForm::class);
-
 Route::get('/PPMP', PPMP::class);
-
-Route::get('/PPMP-form', PpmpForm::class);
-
 Route::get('/BUR', BUR::class);
-
-Route::get('/appropriations', Appropriations::class);
-
 Route::get('/amendment', Amendment::class);
 
-Route::get('/BUR-form', BurForm::class);
 
-Route::get('/amendment-form', AmendmentForm::class);
 
+//Form Routes
+Route::get('/activity-justification-form', ActivityJustificationForm::class);
+Route::get('/personal-services-form', PersonalServicesForm::class);
+Route::get('/personnel-schedule-form', PersonnelScheduleForm::class);
+Route::get('/MOOE-form', MaintenanceForm::class);
 Route::get('/capital-outlay-form', CapitalOutlayForm::class);
+Route::get('/PPMP-form', PpmpForm::class);
+
+
+
+//Others
+Route::get('/home', Chart::class);
+Route::get('/settings', Settings::class);
+Route::get('generate-pdf', [App\Http\Controllers\PDFController::class, 'generatePDF']);
 
 
 Route::get('/test-db', function () {
