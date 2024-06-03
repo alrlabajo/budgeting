74d2c072be92adc4e4ef0ddb4c478dbf04699dd8
Kayne Rodrigo <kayne.rodrigo11@gmail.com>
2024-06-04 05:54:12 +0800

    fixed views/components & added users table

--- app/View/Components/amendmentButton.php
+++ app/View/Components/amendmentButton.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class amendment_buttons extends Component
+class amendmentButton extends Component
 {
     /**
      * Create a new component instance.
@@ -21,6 +21,6 @@ class amendment_buttons extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.amendment-buttons');
+        return view('components.amendment-button');
     }
 }
