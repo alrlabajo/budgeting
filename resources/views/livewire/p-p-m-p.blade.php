ed64c4d39f59dcb5cb56981912230f56f78a0eb2
alrlabajo <alrlabajo@gmail.com>
2024-06-06 06:34:40 +0800

    ppmp records

--- resources/views/livewire/p-p-m-p.blade.php
+++ resources/views/livewire/p-p-m-p.blade.php
@@ -1,20 +1,87 @@
-
-    <x-slot name="title">Project Procurement Management Plan</x-slot>
-    <div class="p-2 sm:ml-64">
-        <div class="p-2">
-            <!-- Header -->
-            <div class="flex w-full pb-5 justify-between">
-                <div class="flex flex-col gap-y-2 text-indigo-800 text-[25px] font-extrabold font-['Inter'] leading-5">Project Procurement Management Plan
-                    <div class="flex flex-row items-center">
-                        <label class="text-yellow-700 text-sm font-normal font-['Inter'] leading-loose mr-1">Budget Call Forms ></label>
-                        <label class="text-black text-sm font-normal font-['Inter'] leading-loose">Project Procurement Management Plan No. </label>
-                    </div>
+<x-slot name="title">Project Procurement Management Plan</x-slot>
+<div class="p-2 sm:ml-64">
+    <div class="p-2">
+        <!-- Header -->
+        <div class="flex w-full pb-5 justify-between">
+            <div class="flex flex-col gap-y-2 text-indigo-800 text-[25px] font-extrabold font-['Inter'] leading-5">Project Procurement Management Plan
+                <div class="flex flex-row items-center">
+                    <label class="text-yellow-700 text-sm font-normal font-['Inter'] leading-loose mr-1">Budget Call Forms ></label>
+                    <label class="text-black text-sm font-normal font-['Inter'] leading-loose">Project Procurement Management Plan No. </label>
                 </div>
-                @include('components.export-button')
-            </div>
-
-            <div class="w-full h-full px-10 py-4 bg-white rounded-lg shadow border border-zinc-300 space-y-4">
-
-
             </div>
         </div>
+
+        <div class="w-full h-full px-4 py-4 bg-white rounded-lg shadow border border-zinc-300 space-y-4">
+            <table id="ppmp-table" class="min-w-full divide-y divide-gray-200 items-center overflow-y-auto">
+                <thead>
+                    <tr>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight" rowspan="2">Class Year</th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight" rowspan="2">Program Title</th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight" rowspan="2">Account Title</th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight" rowspan="2">Account Code</th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight" rowspan="2">Item/Name</th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight" rowspan="2">Description </th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight" rowspan="2">Qty</th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight" rowspan="2">Unit of Issue</th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight" rowspan="2">Unit Price</th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight" rowspan="2">Estimated Budget</th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight" rowspan="2">Procurement Method</th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight whitespace-nowrap" colspan="12">Delivery/Implementation Schedule</th>
+                        <th scope="col" class="relative p-7 border border-slate-300" rowspan="2">
+                            <span class="sr-only">Edit</span>
+                        </th>
+                    </tr>
+                    <tr>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight">J</th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight">F</th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight">M</th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight">A</th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight">M</th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight">J</th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight">J</th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight">A</th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight">S</th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight">O</th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight">N</th>
+                        <th scope="col" class="px-2 py-3 text-black border border-slate-300 text-sm font-bold font-['Inter'] leading-tight">D</th>
+                    </tr>
+                </thead>
+
+                <tbody class="bg-white divide-y divide-gray-200">
+                        <tr class="items-center">
+                            <td class="px-2 py-3 text-sm text-black border border-slate-300"></td>
+                            <td class="px-2 py-3 text-sm text-black border border-slate-300"></td>
+                            <td class="px-2 py-3 text-sm text-black border border-slate-300"></td>
+                            <td class="px-2 py-3 text-sm text-black border border-slate-300"></td>
+                            <td class="px-2 py-3 text-sm text-black border border-slate-300"></td>
+                            <td class="px-2 py-3 text-sm text-black border border-slate-300"></td>
+                            <td class="px-2 py-3 text-sm text-black border border-slate-300"></td>
+                            <td class="px-2 py-3 text-sm text-black border border-slate-300"></td>
+                            <td class="px-2 py-3 text-sm text-black border border-slate-300"></td>
+                            <td class="px-2 py-3 text-sm text-black border border-slate-300"></td>
+                            <td class="px-2 py-3 text-sm text-black border border-slate-300"></td>
+                            <td class="px-2 py-3 text-sm text-black border border-slate-300"></td>
+                            <td class="px-2 py-3 text-sm text-black border border-slate-300"></td>
+                            <td class="px-2 py-3 text-sm text-black border border-slate-300"></td>
+                            <td class="px-2 py-3 text-sm text-black border border-slate-300"></td>
+                            <td class="px-2 py-3 text-sm text-black border border-slate-300"></td>
+                            <td class="px-2 py-3 text-sm text-black border border-slate-300"></td>
+                            <td class="px-2 py-3 text-sm text-black border border-slate-300"></td>
+                            <td class="px-2 py-3 text-sm text-black border border-slate-300"></td>
+                            <td class="px-2 py-3 text-sm text-black border border-slate-300"></td>
+                            <td class="px-2 py-4 text-sm font-medium border border-slate-300"></td>
+                            <td class="px-2 py-4 text-sm font-medium border border-slate-300"></td>
+                            <td class="px-2 py-4 text-sm font-medium border border-slate-300"></td>
+                            <td class="py-4 text-sm font-medium border border-slate-300">
+                                <!-- Edit/Delete -->
+                                <div class="flex flex-col items-center gap-y-2 px-2">
+                                    @include('components.editdelete-button')
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+        </div>
+
+    </div>
+</div>
--- resources/views/livewire/personal-services-form.blade.php
+++ resources/views/livewire/personal-services-form.blade.php
@@ -1,114 +1,113 @@
+<div class="p-2 sm:ml-64">
+    <div class="p-2">
+        <!-- Header -->
+        <div class="w-96 h-9 justify-between items-center inline-flex py-10">
+            <label class="w-96 h-8 absolute text-indigo-800 text-3xl font-extrabold font-['Inter'] leading-9 whitespace-nowrap">Personal Services Document</label>
+        </div>
 
-    <div class="p-2 sm:ml-64">
-        <div class="p-2">
-            <!-- Header -->
-            <div class="w-96 h-9 justify-between items-center inline-flex py-10">
-                <label class="w-96 h-8 absolute text-indigo-800 text-3xl font-extrabold font-['Inter'] leading-9 whitespace-nowrap">Personal Services Document</label>
-            </div>
+        <!-- POST FORM START -->
+        <form wire::submit="submit">
 
-            <!-- POST FORM START -->
-            <form wire::submit="submit">
+            <!-- Container -->
+            <div class="w-full h-full px-6 py-4 bg-white rounded-lg shadow border border-zinc-300 space-y-4">
 
-                <!-- Container -->
-                <div class="w-full h-full px-6 py-4 bg-white rounded-lg shadow border border-zinc-300 space-y-4">
+                <!-- Top Part of Container -->
+                <div class="flex justify-between py-6">
 
-                    <!-- Top Part of Container -->
-                    <div class="flex justify-between py-6">
+                    <div class="flex flex-col space-y-3">
 
-                        <div class="flex flex-col space-y-3">
-
-                            <!-- Select College/Office -->
-                            <div class="w-80 rounded-lg">
-                                <select wire:model="college_office" id="college_office" name="college_office" class="block w-full h-10 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" style="text-indent: 10px;">
-                                    <option>Select College/Office</option>
-                                    <option value="CISTM">CISTM</option>
-                                    <option value="College 2">College 2</option>
-                                    <!-- Other options -->
-                                </select>
-                                @error('college_office')
-                                <span class="text-red-500">{{ $message }}</span>
-                                @enderror
-                            </div>
-
-                            <input type="date" class="w-80 h-10 rounded-lg border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="School Year">
+                        <!-- Select College/Office -->
+                        <div class="w-80 rounded-lg">
+                            <select wire:model="college_office" id="college_office" name="college_office" class="block w-full h-10 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" style="text-indent: 10px;">
+                                <option>Select College/Office</option>
+                                <option value="CISTM">CISTM</option>
+                                <option value="College 2">College 2</option>
+                                <!-- Other options -->
+                            </select>
+                            @error('college_office')
+                            <span class="text-red-500">{{ $message }}</span>
+                            @enderror
                         </div>
+
+                        <input type="date" class="w-80 h-10 rounded-lg border-gray-300 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="School Year">
                     </div>
-
-                    @csrf
-                    <table id="personalservices-table" class="w-full text-sm text-left rtl:text-right text-gray-500">
-                        <!-- Header -->
-                        <thead class=" text-zinc-950 text-base font-semibold font-['Inter'] leading-normal gap-y-12">
-                            <tr>
-                                <th scope="col" class="px-6 py-3 whitespace-nowrap">Account Code</th>
-                                <th scope="col" class="pr-10 py-3">Item of Expenditures</th>
-                                <th scope="col" class="px-6 py-3">Proposed Budget</th>
-                                <th scope="col" class="px-6 py-3 text-center">Justification</th>
-                            </tr>
-                        </thead>
-                        <!-- Body -->
-                        <tbody id="personalservices-tbody">
-                            @foreach ($items as $index => $item)
-                            {{-- get the values as array --}}
-                            {{-- @foreach($user as $key => $value) --}}
-                            {{-- input fields --}}
-                            <tr class="gap-6">
-
-                                <th scope="row" class="px-6 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug whitespace-nowrap">
-                                    {{ $item['account_code'] }}
-                                </th>
-
-                                <td class="pr-6 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
-                                    {{ $item['item'] }}
-                                </td>
-                                <td class="px-4 py-2">
-
-                                    {{-- input 1 for proposed budget  --}}
-
-                                    <input type="number" step="0.01" name="budget" wire:model="items.{{ $index }}.budget" class="w-36 h-8 px-3 py-2 rounded-md shadow border border-zinc-200 items-center gap-2 inline-flex bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="₱ 0.00">
-                                    @error('items.' . $index . '.budget')
-                                    <span class="text-red-500">{{ $message }}</span>
-                                    @enderror
-                                </td>
-                                <td class="px-6 py-2">
-
-                                    {{-- input 2 for justification budget  --}}
-                                    <input type="text" wire:model="items.{{ $index }}.justification" class="w-96 h-8 px-3 py-2 rounded-md shadow border border-zinc-200 items-center gap-2 inline-flex bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description">
-                                    @error('items.' . $index . '.justification')
-                                    <span class="text-red-500">{{ $message }}</span>
-                                    @enderror
-                                </td>
-                            </tr>
-                            {{-- @endforeach --}}
-                            @endforeach
-                        </tbody>
-                    </table>
-
-
-                    <!-- Bottom Buttons -->
-                    <div class="flex justify-between py-4">
-                        @include('components.back-button')
-                        <button wire:click.prevent="submit" id="submit-btn" class="w-30 h-10 px-4 py-2 bg-indigo-800 rounded-md shadow justify-center items-center text-white text-base font-medium font-['Inter'] leading-tight">Submit
-                        </button>
-                    </div>
-
                 </div>
 
-            </form>
+                @csrf
+                <table id="personalservices-table" class="w-full text-sm text-left rtl:text-right text-gray-500">
+                    <!-- Header -->
+                    <thead class=" text-zinc-950 text-base font-semibold font-['Inter'] leading-normal gap-y-12">
+                        <tr>
+                            <th scope="col" class="px-6 py-3 whitespace-nowrap">Account Code</th>
+                            <th scope="col" class="pr-10 py-3">Item of Expenditures</th>
+                            <th scope="col" class="px-6 py-3">Proposed Budget</th>
+                            <th scope="col" class="px-6 py-3 text-center">Justification</th>
+                        </tr>
+                    </thead>
+                    <!-- Body -->
+                    <tbody id="personalservices-tbody">
+                        @foreach ($items as $index => $item)
+                        {{-- get the values as array --}}
+                        {{-- @foreach($user as $key => $value) --}}
+                        {{-- input fields --}}
+                        <tr class="gap-6">
 
-            @foreach ($items as $index => $item)
-            <?php
-            echo $item['budget'];
-            echo $item['justification'];
-            ?>
-            @endforeach
+                            <th scope="row" class="px-6 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug whitespace-nowrap">
+                                {{ $item['account_code'] }}
+                            </th>
+
+                            <td class="pr-6 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
+                                {{ $item['item'] }}
+                            </td>
+                            <td class="px-4 py-2">
+
+                                {{-- input 1 for proposed budget  --}}
+
+                                <input type="number" step="0.01" name="budget" wire:model="items.{{ $index }}.budget" class="w-36 h-8 px-3 py-2 rounded-md shadow border border-zinc-200 items-center gap-2 inline-flex bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="₱ 0.00">
+                                @error('items.' . $index . '.budget')
+                                <span class="text-red-500">{{ $message }}</span>
+                                @enderror
+                            </td>
+                            <td class="px-6 py-2">
+
+                                {{-- input 2 for justification budget  --}}
+                                <input type="text" wire:model="items.{{ $index }}.justification" class="w-96 h-8 px-3 py-2 rounded-md shadow border border-zinc-200 items-center gap-2 inline-flex bg-transparent text-zinc-500 text-sm font-normal font-['Inter'] leading-tight" placeholder="Description">
+                                @error('items.' . $index . '.justification')
+                                <span class="text-red-500">{{ $message }}</span>
+                                @enderror
+                            </td>
+                        </tr>
+                        {{-- @endforeach --}}
+                        @endforeach
+                    </tbody>
+                </table>
+
+
+                <!-- Bottom Buttons -->
+                <div class="flex justify-between py-6">
+                    @include('components.back-button')
+                    <button wire:click.prevent="submit" name="submit-btn" class="w-30 h-10 px-4 py-2 bg-indigo-800 rounded-md shadow justify-center items-center text-white text-base font-medium font-['Inter'] leading-tight">Submit
+                    </button>
+                </div>
+
+            </div>
+
+        </form>
+
+        @foreach ($items as $index => $item)
+        <?php
+        echo $item['budget'];
+        echo $item['justification'];
+        ?>
+        @endforeach
 
 
 
-            <!-- Success Message -->
-            @if (session()->has('message'))
-            <th scope="row" class="px-6 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
-                {{ $item['budget'] }}
-            </th>
-            @endif
-        </div>
+        <!-- Success Message -->
+        @if (session()->has('message'))
+        <th scope="row" class="px-6 py-2 text-zinc-950 text-sm font-medium font-['Inter'] leading-snug">
+            {{ $item['budget'] }}
+        </th>
+        @endif
     </div>
+</div>
