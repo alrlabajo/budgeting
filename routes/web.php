b779a7a820c72d22a036a2fa84398dca12f86569
alrlabajo <alrlabajo@gmail.com>
2024-06-05 12:21:21 +0800

    fixed layout

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,33 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Livewire\Chart;
+use App\Livewire\Settings;
+use App\Livewire\ActivityJustification;
+use App\Livewire\ActivityJustificationForm;
+use App\Livewire\PersonalServices;
+use App\Livewire\PersonalServicesForm;
+use App\Livewire\PersonnelSchedule;
+use App\Livewire\PersonnelScheduleForm;
+use App\Livewire\Mooe;
+use App\Livewire\MaintenanceForm;
+use App\Livewire\CapitalOutlay;
+use App\Livewire\CapitalOutlayForm;
+use App\Livewire\PPMP;
+use App\Livewire\PpmpForm;
+use App\Livewire\BUR;
+use App\Livewire\Amendment;
+use App\Livewire\BurForm;
+use App\Livewire\AmendmentForm;
+use App\Livewire\Appropriations;
+use Illuminate\Support\Facades\DB;
+
+use App\Livewire\LoadCapitalOutlay;
+
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\RoleController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +40,74 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+/*Route::get('/', function () {
+    return view('welcome');
+})*/
+
+// roles and permissions route
+
+// Permissions Routes
+Route::resource('permissions', App\Http\Controllers\PermissionController::class);
+Route::put('permissions/{permissionId}/edit', [App\Http\Controllers\PermissionController::class,'edit']);
+Route::get('permissions/{permissionId}/delete', [App\Http\Controllers\PermissionController::class,'destroy']);
+
+// Roles Routes
+Route::resource('roles',App\Http\Controllers\RoleController::class);
+Route::put('roles/{roleId}/edit', [App\Http\Controllers\RoleController::class,'edit']);
+Route::get('roles/{roleId}/delete', [App\Http\Controllers\RoleController::class,'destroy']);
+
+// Users Routes
+Route::resource('users',App\Http\Controllers\UserController::class);
+Route::put('users/{userId}/edit',[App\Http\Controllers\UserController::class,'edit']);
+Route::get('users/{userId}/delete', [App\Http\Controllers\UserController::class, 'destroy']);
+
+
+// end of roles and permissions route
+
+
+
+//Record Routes
+Route::get('/activity-justification', ActivityJustification::class);
+Route::get('/personal-services', PersonalServices::class);
+Route::get('/personnel-schedule', PersonnelSchedule::class);
+Route::get('/MOOE', Mooe::class);
+Route::get('/capital-outlay', LoadCapitalOutlay::class);
+Route::get('/PPMP', PPMP::class);
+Route::get('/BUR', BUR::class);
+Route::get('/amendment', Amendment::class);
+
+
+
+
+//Form Routes
+Route::get('/activity-justification-form', ActivityJustificationForm::class);
+Route::get('/personal-services-form', PersonalServicesForm::class);
+Route::get('/personnel-schedule-form', PersonnelScheduleForm::class);
+Route::get('/MOOE-form', MaintenanceForm::class);
+Route::get('/capital-outlay-form', CapitalOutlayForm::class);
+Route::get('/PPMP-form', PpmpForm::class);
+Route::get('/appropriations', Appropriations::class);
+Route::get('/BUR-form', BurForm::class);
+Route::get('/amendment-form', AmendmentForm::class);
+
+
+
+//Others
+Route::get('/chart', Chart::class);
+Route::get('/settings', Settings::class);
+Route::get('generate-pdf', [App\Http\Controllers\PDFController::class, 'generatePDF']);
+
+
+
+Route::get('/test-db', function () {
+    try {
+        DB::connection()->getPdo();
+        return 'Database connection is working';
+    } catch (\Exception $e) {
+        return 'Database connection is not working: ' . $e->getMessage();
+    }
+});
 
 Route::get('/', function () {
     return view('welcome');
@@ -28,4 +123,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require _DIR_.'/auth.php';
