7c7683c5b1448440e358e09de3eb1a586ce3ea2a
alrlabajo <alrlabajo@gmail.com>
2024-06-05 17:51:25 +0800

    fixed layouts and functionalities

--- resources/views/components/back-button.blade.php
+++ resources/views/components/back-button.blade.php
@@ -1,3 +1,10 @@
 <div>
-    <button onclick="window.history.back()" class="w-30 h-10 px-4 py-2 bg-gray-500 rounded-md shadow justify-center items-center text-white text-base font-medium font-['Inter'] leading-tight">Back</button>
+    <button id="backButton" class="w-30 h-10 px-4 py-2 bg-gray-500 rounded-md shadow justify-center items-center text-white text-base font-medium font-['Inter'] leading-tight">Back</button>
+
+    <script>
+        document.getElementById("backButton").addEventListener("click", function() {
+            window.history.back();
+        });
+    </script>
+
 </div>
