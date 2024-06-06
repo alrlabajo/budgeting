2c3d63189ddcb2d73b034daa357c43687dd17ee9
alrlabajo <alrlabajo@gmail.com>
2024-06-06 20:50:04 +0800

    revised dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,124 +4,78 @@
     <div class="p-2 sm:ml-64">
         <div class="p-2 space-y-4">
             <div class="flex flex-col sm:flex-row space-x-0 sm:space-x-2 space-y-2 sm:space-y-0">
-                <!-- WELCOME! -->
-                <div class="relative max-w-md max-h-lg rounded-lg shadow border border-gray border-opacity-50 bg-cover bg-center w-full">
-                    <div class="absolute inset-0 rounded-lg"></div>
-                    <img class="max-w-md max-h-lg opacity-40 mx-auto mt-auto" src="css/images/background.png">
-                    <div class="absolute inset-0 flex flex-col items-start ml-6 justify-top text-left left-2 mt-8">
-                        <div class="flex items-center">
-                            <img class="w-[50px] h-[50px]" src="css/images/image 5.png">
-                            <img class="h-[40px] ml-2" src="css/images/image 4.png">
+                <div class="flex flex-col sm:flex-row space-x-0 sm:space-x-2 space-y-2 sm:space-y-0">
+                    <!-- WELCOME! -->
+                    <div class="relative max-w-xl rounded-lg shadow border border-gray border-opacity-50 bg-cover bg-center w-full">
+                        <div class="absolute inset-0 rounded-lg"></div>
+                        <img class="w-[649px] h-[385px] opacity-40 mx-auto mt-auto" src="css/images/background.png">
+                        <div class="absolute inset-0 flex flex-col items-start ml-6 justify-top text-left left-2 mt-8">
+                            <div class="flex items-center">
+                                <img class="w-[50px] h-[50px]" src="css/images/image 5.png">
+                                <img class="h-[40px] ml-2" src="css/images/image 4.png">
+                            </div>
+                            <h1 class="text-2xl font-extrabold ml-2 mt-3 mb-3 text-blue-800 z-10 relative">PLM Budget System</h1>
+                            <h3 class="text-[55px] font-extrabold ml-2 mb-1 text-blue-800 z-10 relative">Welcome,</h3>
+                            <h2 class="text-2xl font-bold ml-2 text-cyan-800 z-10 relative">{{ Auth::user()?->name }}.</h2>
                         </div>
-                        <h1 class="text-2xl font-extrabold ml-2 mt-3 mb-3 text-blue-800 z-10 relative">PLM Budget System</h1>
-                        <h3 class="text-[55px] font-extrabold ml-2 mb-1 text-blue-800 z-10 relative">Welcome,</h3>
-                        <h2 class="text-2xl font-bold ml-2 text-cyan-800 z-10 relative">{{ Auth::user()?->name }}.</h2>
                     </div>
-                </div>
-                <div class="flex flex-col space-y-4 w-full">
-                    <!-- Budget Call Forms -->
-                    <div class="max-w-xl h-56 md:h-56 sm:h-64 rounded-lg shadow border border-gray border-opacity-20 justify-center place-items-center w-full">
-                        <!-- Header -->
-                        <div class="flex items-center px-4 py-3">
-                            <img class="w-[30px] h-[30px] mr-2" src="css/images/icon1.png">
-                            <h1 class="text-xl font-extrabold text-blue-800 px-1">Budget Call Forms</h1>
-                        </div>
-                        <!-- 1st Row -->
-                        <div class="flex-col justify-items-center grid grid-col-3 md:grid-col-2 sm:grid-col-2 gap-x-3 gap-y-1 items-center content-center px-2 pt-2">
-                            <div class="grid grid-cols-3 md:grid-cols-3 sm:grid-cols-2 gap-x-2 gap-y-2 md:gap-x-2 sm:gap-x-2">
-                                <!-- Activity Justification Document -->
-                                <div class="flex items-center justify-center w-30 h-16 bg-blue-800 text-[13px] text-white rounded-md">
-                                    <a href="/activity-justification-form" class="text-xs text-white btn text-center">Activity Justification</a>
-                                </div>
-                                <!-- Personnel Schedule -->
-                                <div class="flex items-center justify-center w-30 h-16 bg-blue-800 text-[13px] text-white rounded-md">
-                                    <a href="/personnel-schedule-form" class="text-sm text-white btn text-center">Personnel Schedule</a>
-                                </div>
-                                <!-- Project Procurement Management Plan -->
-                                <div class="flex items-center justify-center w-30 h-16 bg-blue-800 text-[13px] text-white rounded-md">
-                                    <a href="/PPMP-form" class="text-xs text-white btn text-center">Project Procurement Management Plan</a>
-                                </div>
-                                <!--Personal Services-->
-                                <div class="flex items-center justify-center w-30 h-16 bg-blue-800 text-[13px] text-white rounded-md">
-                                    <a href="/personal-services-form" class="text-sm text-white btn text-center">Personal Services</a>
-                                </div>
-                                <!-- Maintenance and Other Operating Expenses -->
-                                <div class="flex items-center justify-center w-30 h-16 bg-blue-800 text-[13px] text-white rounded-md">
-                                    <a href="/MOOE-form" class="text-xs text-white btn text-center">Maintenance and Other Operating Expenses</a>
-                                </div>
-                                <!-- Capital Outlay -->
-                                <div class="flex items-center justify-center w-30 h-16 bg-blue-800 text-white rounded-md">
-                                    <a href="/capital-outlay-form" class="text-sm text-white btn text-center">Capital Outlay</a>
+                    <div class="flex flex-col space-y-4 w-full">
+                        <!-- Budget Call Forms -->
+                        <div class="max-w-2xl h-56 md:h-56 sm:h-64 rounded-lg shadow border border-gray border-opacity-20 justify-center place-items-center w-full">
+                            <!-- Header -->
+                            <div class="flex items-center px-4 py-3">
+                                <img class="w-[30px] h-[30px] mr-2" src="css/images/icon1.png">
+                                <h1 class="text-xl font-extrabold text-blue-800 px-1">Budget Call Forms</h1>
+                            </div>
+                            <!-- 1st Row -->
+                            <div class="flex-col justify-items-center grid grid-col-3 md:grid-col-2 sm:grid-col-2 gap-x-3 gap-y-1 items-center content-center px-2 pt-2">
+                                <div class="grid grid-cols-3 md:grid-cols-3 sm:grid-cols-2 gap-x-2 gap-y-2 md:gap-x-2 sm:gap-x-2">
+                                    <!-- Activity Justification Document -->
+                                    <div class="flex items-center justify-center w-30 h-16 bg-blue-800 text-[13px] text-white rounded-md">
+                                        <a href="/activity-justification-form" class="text-xs text-white btn text-center">Activity Justification</a>
+                                    </div>
+                                    <!-- Personnel Schedule -->
+                                    <div class="flex items-center justify-center w-30 h-16 bg-blue-800 text-[13px] text-white rounded-md">
+                                        <a href="/personnel-schedule-form" class="text-sm text-white btn text-center">Personnel Schedule</a>
+                                    </div>
+                                    <!-- Project Procurement Management Plan -->
+                                    <div class="flex items-center justify-center w-30 h-16 bg-blue-800 text-[13px] text-white rounded-md">
+                                        <a href="/PPMP-form" class="text-xs text-white btn text-center">Project Procurement Management Plan</a>
+                                    </div>
+                                    <!--Personal Services-->
+                                    <div class="flex items-center justify-center w-30 h-16 bg-blue-800 text-[13px] text-white rounded-md">
+                                        <a href="/personal-services-form" class="text-sm text-white btn text-center">Personal Services</a>
+                                    </div>
+                                    <!-- Maintenance and Other Operating Expenses -->
+                                    <div class="flex items-center justify-center w-30 h-16 bg-blue-800 text-[13px] text-white rounded-md">
+                                        <a href="/MOOE-form" class="text-xs text-white btn text-center">Maintenance and Other Operating Expenses</a>
+                                    </div>
+                                    <!-- Capital Outlay -->
+                                    <div class="flex items-center justify-center w-30 h-16 bg-blue-800 text-white rounded-md">
+                                        <a href="/capital-outlay-form" class="text-sm text-white btn text-center">Capital Outlay</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <!-- Payment Request -->
-                    <div class="max-w-xl h-36 md:h-36 sm:h-72 rounded-lg shadow border border-gray border-opacity-20 justify-center place-items-center w-full">
-                        <div class="flex items-center px-4 py-3">
-                            <img class="w-[30px] h-[30px] mr-2" src="css/images/icon1.png">
-                            <h1 class="text-xl font-extrabold text-blue-800">Payment Request Forms</h1>
-                        </div>
-                        <div class="grid grid-cols-2 px-2 md:gap-x-1 md:grid-cols-2 sm:grid-cols-1 justify-items-center sm:py-2 md:gap-y-2 sm:gap-y-2 sm:gap-x-4">
-                            <div class="flex items-center justify-center w-56 h-16  bg-blue-800 text-white rounded-md">
-                                <a href="/BUR-form" class="text-sm text-white btn text-center">Budget Utilization Request</a>
+                        <!-- Payment Request -->
+                        <div class="max-w-2xl h-36 md:h-36 sm:h-72 rounded-lg shadow border border-gray border-opacity-20 justify-center place-items-center w-full">
+                            <div class="flex items-center px-4 py-3">
+                                <img class="w-[30px] h-[30px] mr-2" src="css/images/icon1.png">
+                                <h1 class="text-xl font-extrabold text-blue-800">Payment Request Forms</h1>
                             </div>
-                            <div class="flex items-center justify-center w-56 h-16 bg-blue-800 text-white rounded-md">
-                                <a href="/amendment-form" class="text-sm text-white btn text-center">Amendment Form</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Recent Activities -->
-                <div class="max-w-fit max-h-md px-2 md:w-[290px] md:max-w-xl sm:max-w-2xl items-center whitespace-nowrap rounded-lg shadow border border-gray border-opacity-20 w-full">
-                    <div class="flex items-center border-b-2">
-                        <img class="w-[18px] h-[18px] ml-3" src="css/images/icon2.png">
-                        <h1 class="flex items-top justify-left text-[15px] font-bold ml-1 mt-2 mb-2">Recent Activities</h1>
-                    </div>
-                    <!-- NEED FUNCTIONALITY -->
-                    <div class="flex h-20 border-b-2">
-                        <div class="p-1.5">
-                            <h1 class="flex items-top justify-left text-[15px] font-extrabold">Maribeth Solquio</h1>
-                            <h2 class="flex items-top justify-left text-[11px] font-bold">Edited Status of Appropriations</h2>
-                            <div class="flex items-top justify-between">
-                                <h3 class="flex items-top text-[11px]">15 minutes ago</h3>
-                                <a href="#" class="flex items-top text-[11px] text-blue-800 underline ml-[120px] btn">View form</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flex h-20 border-b-2">
-                        <div class="p-1.5">
-                            <h1 class="flex items-top justify-left text-[15px] font-extrabold">Maribeth Solquio</h1>
-                            <h2 class="flex items-top justify-left text-[11px] font-bold">Edited Status of Appropriations</h2>
-                            <div class="flex items-top justify-between">
-                                <h3 class="flex items-top text-[11px]">15 minutes ago</h3>
-                                <a href="#" class="flex items-top text-[11px] text-blue-800 underline ml-[120px] btn">View form</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flex h-20 border-b-2">
-                        <div class="p-1.5">
-                            <h1 class="flex items-top justify-left text-[15px] font-extrabold">Maribeth Solquio</h1>
-                            <h2 class="flex items-top justify-left text-[11px] font-bold">Edited Status of Appropriations</h2>
-                            <div class="flex items-top justify-between">
-                                <h3 class="flex items-top text-[11px]">15 minutes ago</h3>
-                                <a href="#" class="flex items-top text-[11px] text-blue-800 underline ml-[120px] btn">View form</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flex h-20">
-                        <div class="p-1.5">
-                            <h1 class="flex items-top justify-left text-[15px] font-extrabold">Maribeth Solquio</h1>
-                            <h2 class="flex items-top justify-left text-[11px] font-bold">Edited Status of Appropriations</h2>
-                            <div class="flex items-top justify-between">
-                                <h3 class="flex items-top text-[11px]">15 minutes ago</h3>
-                                <a href="#" class="flex items-top text-[11px] text-blue-800 underline ml-[120px] btn">View form</a>
+                            <div class="grid grid-cols-2 px-6 md:gap-x-1 md:grid-cols-2 sm:grid-cols-1 justify-items-center sm:py-2 md:gap-y-2 sm:gap-y-2 sm:gap-x-4">
+                                <div class="flex items-center justify-center w-72 h-16  bg-blue-800 text-white rounded-md">
+                                    <a href="/BUR-form" class="text-sm text-white btn text-center">Budget Utilization Request</a>
+                                </div>
+                                <div class="flex items-center justify-center w-72 h-16 bg-blue-800 text-white rounded-md">
+                                    <a href="/amendment-form" class="text-sm text-white btn text-center">Amendment Form</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="flex flex-col sm:flex-row space-x-0 sm:space-x-4 space-y-4 sm:space-y-0">
+            <div class="flex flex-row sm:flex-row space-x-0 sm:space-x-4 space-y-4 sm:space-y-0">
                 <!-- Status of Appropriations -->
                 <div class="flex flex-col max-w-2xl max-h-xl rounded-lg shadow border border-gray border-opacity-20 w-full">
                     <!-- Title -->
