607951e117511cbeede19b4ec1b9dc4250ca309c
Kayne Rodrigo <kayne.rodrigo11@gmail.com>
2024-06-08 02:29:04 +0800

    temporarily disabled login page

--- routes/web.php
+++ routes/web.php
@@ -128,11 +128,11 @@ Route::get('/test-db', function () {
 
 Route::get('/', function () {
     return view('dashboard');
-});
+})->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->name('dashboard');
 
 Route::get('/sample-csv', function () {
     $headers = [
